34b1c0ca3ec79ba80769aa85c1b6a116171dea61
Lilia Futekova <futekoval@gmail.com>
2020-10-14 14:37:22 +0300

    aufgabe 5

---
+++ src/exercise3/AngleCalculation.java
@@ -0,0 +1,28 @@
+package exercise3;
+
+import java.util.Scanner;
+
+public class AngleCalculation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double calculatedAngle = 0;
+        char type;
+        char newType = ' ';
+
+        do{
+            double initialAngle = input.nextDouble();
+            String typeString = input.next();
+            type = typeString.charAt(0);
+
+            if(type == 'd'){
+                calculatedAngle = (initialAngle * Math.PI) / 180;
+                newType = 'r';
+            }else if(type == 'r'){
+                calculatedAngle = (initialAngle * 180) / Math.PI;
+                newType = 'd';
+            }
+
+            System.out.println("Angle: " + calculatedAngle + newType);
+        }while(type == 'd' && type == 'r');
+    }
+}
